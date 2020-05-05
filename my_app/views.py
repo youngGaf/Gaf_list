ea5d9d5e91dffd4650d325836d6133d949766374
gafar femi <youngabuhafiz@gmail.com>
2020-05-05 06:47:50 +0100

    Add frontend and requirements

--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,13 @@
+import requests 
 from django.shortcuts import render
-
+from bs4 import BeautifulSoup
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
+
+def new(request):
+    search = request.POST.get('search')
+    stuff_for_frontend = {
+        'search': search
+    }
+    return render(request, 'my_app/new.html', stuff_for_frontend)
