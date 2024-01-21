23f97c177abfef8c30aee4f8ce8aa44773b4eedf
youngGaf <youngabuhafiz@gmail.com>
2024-01-21 12:09:28 -0800

    fix: scraping routes

--- my_app/views.py
+++ my_app/views.py
@@ -25,19 +25,20 @@ def new(request):
     soup = BeautifulSoup(data, features = 'html.parser')
     
     # extract all information engaraved in a-tags and a result-title class
-    post_listings = soup.find_all('li', {'class': 'result-row'})
+    post_listings = soup.find_all('li', {'class': 'cl-static-search-result'})
+
 
     final_postings = []
     for post in post_listings:
-        post_title = post.find(class_= 'result-title').text
+        post_title = post.find(class_= 'title').text
         post_url = post.find('a').get('href')
         
-        if post.find(class_= 'result-price'):
-            post_price = post.find(class_= 'result-price').text
+        if post.find(class_= 'price'):
+            post_price = post.find(class_= 'price').text
         else:
             post_price = 'N/A'
         
-        if post.find(class_ = 'result-image').get('data-ids'):
+        if post.find(class_= 'result-image'):
             post_image_id = post.find(class_ = 'result-image').get('data-ids').split(',')[0].split(':')[1]
             post_image_url = BASE_IMAGE_URL.format(post_image_id)
         else:
--- readme.md
+++ readme.md
@@ -1,1 +1,15 @@
-This is a Craiglist clone... called gaf_list
+This is a Craigslist clone...
+
+## Dependencies
+ - Django
+ - Postgres
+ - pgAdmin
+
+
+
+## Installation Process
+
+1. Create virtual environment -`python -m venv ~/.virtualenvs/djangodev`
+2. Activate virtual environment - `source ~/.virtualenvs/djangodev/bin/activate`
+3. Install django and all dependencies - ` python -m pip install django` `pip install -r requirements.txt`
+4. Run server `python manage.py runserver`
